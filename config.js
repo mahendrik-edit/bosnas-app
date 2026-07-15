28cf5beec3d9308984f84e1e28cea633c3c614dd
mahendrik-edit <mahendrik@gmail.com>
2026-07-15 09:38:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // URL Web App dari Google Apps Script
     // Copy dari hasil deploy Apps Script
-    webAppUrl: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+    webAppUrl: 'https://script.google.com/macros/s/AKfycbzWBPdn2_N8QY7bz2oDz0stBlaSI6Gd-is234zkU5kTqjVddQHyGl_f3x70V-EFCuXryQ/exec',
     
     // Spreadsheet ID (opsional, untuk fallback)
-    spreadsheetId: 'YOUR_SPREADSHEET_ID',
+    spreadsheetId: '1UEamMydVptgCs1YnJIik5fVbFvZXgwY-mv5QyYOHqH4',
     
     // API Key (opsional, untuk fallback)
-    apiKey: 'YOUR_API_KEY'
+    apiKey: 'AIzaSyAiVL0pz7UD9qYnYTd6cmGey5zkUseTBNE'
 };
